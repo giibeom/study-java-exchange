4cb049280bce437a63c0cb934aa46911602cc9a5
jeunghoonsong <thdwmdgns@naver.com>
2023-05-02 20:43:34 +0900

    init

---
+++ src/test/java/woowaapplication/pair/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package woowaapplication.pair.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
