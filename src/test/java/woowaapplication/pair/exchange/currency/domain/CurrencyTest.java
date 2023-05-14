8ccd5da6f8f5d8fc90dba81bad626140438c1e09
giibeom <kpmyung@gmail.com>
2023-05-14 23:24:32 +0900

    test: 수취 국가와 송금액 정보가 없는 예외 케이스을 수정한다

--- src/test/java/woowaapplication/pair/exchange/currency/domain/CurrencyTest.java
+++ src/test/java/woowaapplication/pair/exchange/currency/domain/CurrencyTest.java
@@ -2,10 +2,9 @@ package woowaapplication.pair.exchange.currency.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -17,13 +16,13 @@ class CurrencyTest {
 
     @ParameterizedTest
     @CsvSource(delimiter = ',',
-        value = {
-            "USA,KOREA",
-            "USA,JAPAN",
-            "USA,PHILIPPINES"
-        }
+            value = {
+                    "USA,KOREA",
+                    "USA,JAPAN",
+                    "USA,PHILIPPINES"
+            }
     )
-    public void calcCurrencyResultTest (String src, String dst){
+    public void calcCurrencyResultTest(String src, String dst) {
 
         //given
         Nations srcNations = Nations.valueOf(src);
@@ -40,8 +39,9 @@ class CurrencyTest {
         assertThat(result).isEqualTo(amount * CurrencyFixture.get환율By(dstNations));
     }
 
+    @DisplayName("수취 국가와 송금액 정보가 없을 경우 예외를 던진다")
     @Test
-    public void calcCurrencyBeforeSettingThrows() {
+    void calcCurrencyBeforeSettingThrows() {
         //given
         Nations srcNations = Nations.USA;
         Map<String, Double> quotes = CurrencyFixture.getQuotes(srcNations);
@@ -49,11 +49,9 @@ class CurrencyTest {
         //when
         currency = Currency.of(srcNations, quotes);
 
-        // TODO: 2023/05/14 Currency 획득 후 dst정보가 없을때 저희가 예상한 @IllegalStateException이 아닌, NPE가 발생합니다. 로직을 수정해야 하지 않을까요?
-
         //then
         assertThatThrownBy(
-            () -> currency.calculateDestinationCurrency()
-        ).isInstanceOf(NullPointerException.class);
+                () -> currency.calculateDestinationCurrency()
+        ).isInstanceOf(IllegalStateException.class);
     }
 }
